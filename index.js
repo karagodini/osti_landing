d0b937951bd50a641dc7d1b866d650787197c844
karagodini <karagodini@yandex.ru>
2023-03-10 14:25:26 +0300

    osti

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const btn = document.querySelector(".style-btn");
+const btns = document.querySelectorAll("a");
 const form = document.querySelector(".form");
 const close = document.querySelector(".close");
 
-btn.addEventListener('click', function(e) {
-    e.preventDefault();
-    form.classList.add("show");
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        form.classList.add("show");
+    })
 })
+
 
 close.addEventListener('click', function(s) {
     s.preventDefault();
     form.classList.remove("show");
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+        fetch('/send.php', {
+            method: 'POST',
+            body: formData
+        })
 })
